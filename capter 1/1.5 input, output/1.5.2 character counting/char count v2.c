053351218247eb39eda4469d6a4dc4f017b87f21
Colonia-duck <tomclarke2006@hotmail.com>
2023-11-26 15:14:07 +0000

    section 1.5.2

---
+++ capter 1/1.5 input, output/1.5.2 character counting/char count v2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/*count charactors in input; version 2*/
+
+int main()
+{
+    double nc;
+
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
+    return 0;
+}
