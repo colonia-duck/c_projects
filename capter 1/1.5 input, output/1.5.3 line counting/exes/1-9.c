474b0201c24de81b80d87e1c84c416efc29565bb
Colonia-duck <tomclarke2006@hotmail.com>
2023-11-29 22:16:43 +0000

    exec 1.9

---
+++ capter 1/1.5 input, output/1.5.3 line counting/exes/1-9.c
@@ -0,0 +1,28 @@
+/*  write a program to cpoy its input to its output replacing every 1 or more space with one space*/
+
+#include<stdio.h>
+
+int main()
+{
+    int c, blanks = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c != ' ')
+        {
+            blanks = 0;
+        }
+        if (c == ' ')
+        {
+            ++blanks;
+        }
+        if(blanks > 1)
+            ;
+         
+        else
+        {
+            putchar(c);
+        }
+
+    }
+}
