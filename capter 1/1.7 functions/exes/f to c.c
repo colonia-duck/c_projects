ed13a194a4aa4f5dda78aae9aaf6122f13ae2090
colonia-duck <tomclarke2006@hotmail.com>
2023-12-13 19:07:51 +0000

    section 1.7 exe 1-15

---
+++ capter 1/1.7 functions/exes/f to c.c
@@ -0,0 +1,35 @@
+//exercise 1-15
+/* write a programe that converts fahrenhtight te celces 
+    using functions */
+
+#include<stdio.h>
+
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
+float convert(float fahr)
+{
+    float celces;
+
+    celces = (5.0 / 9.0) * (fahr - 32);
+
+    return celces;
+}
+
+int main()
+{
+    float fhar, celces;
+    fhar = LOWER;
+
+    printf("%s %s\n", "Fahrenheit", "Celces");
+
+    while (fhar <= UPPER)
+    {
+        celces = convert(fhar);
+        printf("%6.0f %9.1f\n",fhar, celces );
+        fhar = fhar + STEP;
+    }
+    
+
+}
